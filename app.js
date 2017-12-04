d506e1c58598d05a7a5209b6d11bad55d6c40877
Alexander Mori <almoriex@gmail.com>
2017-12-04 12:55:27 -0800

    done

---
+++ app.js
@@ -0,0 +1,147 @@
+#!/usr/bin/node
+
+/** */
+/** Dependencies */
+var express = require('express');
+var http = require('http');
+var bodyparser = require('body-parser');
+var fs = require('fs');
+var jsonfile = require('jsonfile');
+var bcrypt = require('bcrypt');
+var speakeasy = require("speakeasy");
+var QRCode = require("qrcode");
+var session = require('express-session');
+
+/** Stores users as JSON */
+var userfile = "users.txt";
+
+//Bcrypt salt rounds
+var saltrounds = 10;
+
+/** Initialize express.js and its templates */
+var app = express();
+app.use(bodyparser.urlencoded({ extended: true }));
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.use(session({
+	secret: 'node application',
+	resave: true,
+	saveUninitialized: true
+}));
+
+/** Initialize the Web Server */
+http.createServer(app).listen("8080", function () {
+	console.log("Web Server is listening on port 8080");
+});
+
+/** Default Page
+		Renders template from view/login.pug
+ */
+app.get('/', function(req, res) {
+	res.render('login');
+});
+
+/** Checks credentials when POST to /login-check
+		Parses JSON file for usernames
+		Compares post data with stored data
+ */
+app.post('/login-check', function(req, res) {
+
+/** Load User "database" */
+	jsonfile.readFile(userfile, function(er, data) {
+
+/** Returns filtered object.
+		Should return exactly one object when username and password match an entry.
+ */
+		var found = data.filter(function(item) {
+			return item.name == req.body.name;
+		});
+
+		if (found.length == 0) {
+			res.send("Login failed because of wrong password or non-existing account.");
+		} else {
+			if (req.body.password == found[0].password) {
+				if (found[0].twofactor == "disabled") {
+					res.send("Login successful!");
+				} else {
+					res.render('verify');
+				}
+			} else {
+				bcrypt.compare(req.body.password, found[0].password, function(err, result) {
+					if (result) {
+						req.session.secret = found[0].twofactor;
+
+						if (found[0].twofactor == "disabled") {
+							res.send("Login successful!");
+						} else {
+							res.render('verify');
+						}
+					} else if (err) {
+						res.send("Login failed because of wrong password or non-existing account.");
+					} else {
+						res.send("Login failed because of wrong password or non-existing account.");
+					}
+				});
+			}
+		}
+	});
+});
+
+app.post("/verify", function(req, resp) {
+	var usertoken = req.body.token;
+	var verified = speakeasy.totp.verify({
+		secret: req.session.secret.base32,
+		encoding: 'base32',
+		token: usertoken
+	});
+	if (verified) {
+		resp.send("Login successful!");
+	} else {
+		resp.send("<script>alert('"
+		+ 'Invalid token, please try again.'
+		+ "')</script>");
+	}
+});
+
+/** New user page. Renders template from views/newuser.pug */
+app.get('/add-users', function(req, res) {
+	res.render('newuser');
+});
+
+/** Current users page.
+ 		Reads userfile "database".
+		Renders template from views/users.pug by passing the users object
+ */
+app.get('/users', function(req, res) {
+	jsonfile.readFile(userfile, function(err, obj){
+		if (err) throw err;
+		res.render('users', { users: obj });
+	});
+});
+
+/** Add users page.
+ 		Reads userfile "database".
+		Appends JSON object with POST data to the old userfile object.
+		Writes new object to the userfile.
+ */
+
+app.post('/adduser', function(req, res) {
+// storing users in file
+	bcrypt.genSalt(saltrounds, function(err, salt) {
+		bcrypt.hash(req.body.password, salt, function(err, hash) {
+			var secret = speakeasy.generateSecret({length: 20});
+			var userdata = { name: req.body.name, password: hash, twofactor: secret};
+
+			QRCode.toDataURL(secret.otpauth_url, function(err, data_url) {
+				jsonfile.readFile(userfile, function(er, data) {
+					data.push(userdata);
+					jsonfile.writeFile(userfile, data, (err) => {
+						res.send('successfully registered new user...<br>' 
+						+ '<a href="/users">Back to User List</a>'
+						+ '<img src="' + data_url + '">');
+					});
+				});
+			});
+		});
+	});
+});
